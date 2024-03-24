6a78326a039b09563e53e721293de040797bf69d
ZeGmX <lendy.mulot@gmail.com>
2024-03-24 16:52:12 +0100

    Database improvements: dicts inside dicts are now stored in subfolders, and only the necessary data is saved on changes

--- src/my_database.py
+++ src/my_database.py
@@ -11,30 +11,38 @@ class Element:
     """
 
     element = None
-    db_key = ""
 
-    def __init__(self, db_key, element):
-        self.db_key = db_key
+    parent_dict = None  # parent information to optimize data saving on changes
+    parent_key = ""
+
+    stop_parent_propagation = False  # If a dict is in a list, we have to save the whole list, not a specific dict key, so we don't propagate the parent
+
+    def __init__(self, parent, parent_key, element, stop_parent_propagation=False):
+        self.parent_dict = parent
+        self.parent_key = parent_key
         self.element = element
-
+        self.stop_parent_propagation = stop_parent_propagation
+        
 
     @staticmethod
-    def convert_to_element(item, db_key):
+    def convert_to_element(item, parent_dict, parent_key, stop_parent_propagation=False):
         """
         Converts a general database item to an Element_list or Element_dict
         No effect if the item is not a list or a dict
         --
         input:
             item: any
-            db_key: str
+            parent_dict: Element_dict
+            parent_key: str
         """
         if type(item) == list:
-            return Element_list(db_key, item)
+            return Element_list(parent_dict, parent_key, item)
         elif type(item) == dict:
-            return Element_dict(db_key, item)
+            return Element_dict(parent_dict, parent_key, item, stop_parent_propagation)
         else:
             return item
         
+
     @staticmethod
     def convert_from_element(item):
         """
@@ -66,16 +74,24 @@ class Element:
         assert db._check_if_acceptable(value), "Value could not be set because it contains an unacceptable type"
         assert db._check_if_acceptable(key), "Key could not be set because it contains an unacceptable type"
         self.element[key] = value
-        db._save_key(self.db_key, db.folder)
+
+        if self.parent_dict is not None:
+            self.parent_dict.save_key(self.parent_key, db.folder) 
+        else: # if the element is the root of the database, it's necessarily a dict
+            self.save_key(self.parent_key, db.folder) 
 
 
     def __delitem__(self, key) -> None:
         del self.element[key]
-        db._save_key(self.db_key, db.folder)
+
+        if self.parent_dict is not None:
+            self.parent_dict.save_key(self.parent_key, db.folder)
+        else: # if the element is the root of the database, it's necessarily a dict
+            self.save_key(self.parent_key, db.folder)
     
 
     def __getitem__(self, key):
-        return Element.convert_to_element(self.element[key], self.db_key)
+        return Element.convert_to_element(self.element[key], self.parent_dict, self.parent_key, self.stop_parent_propagation)
 
 
     def __str__(self) -> str:
@@ -98,14 +114,30 @@ class Element:
         return item in self.element
 
 
+    def get_folder_path(self, root_folder):
+        """
+        Returns the path to the subfolder where the element is stored
+        --
+        input:
+            root_folder: str -> the folder where the whole database is stored
+        --
+        output:
+            res: str
+        """
+        if self.parent_dict is None:
+            return root_folder
+
+        return self.parent_dict.get_folder_path(root_folder) + f"/{self.parent_key}"
+
+
 class Element_list(Element):
     """
     Class for mutable lists in the database
     """
 
-    def __init__(self, db_key, element):
+    def __init__(self, parent_dict, parent_key, element):
         assert type(element) == list
-        super().__init__(db_key, element)
+        super().__init__(parent_dict, parent_key, element, stop_parent_propagation=True)
 
 
     def __add__(self, other):
@@ -114,8 +146,11 @@ class Element_list(Element):
         --
         input:
             other: list
+        --
+        output:
+            res: Element_list
         """
-        return self.element + other
+        return Element_list(self.parent_dict, self.parent_key, self.element + other)
     
     
     def append(self, item):
@@ -127,7 +162,8 @@ class Element_list(Element):
         """
         assert db._check_if_acceptable(item), "Value could not be appended because it contains an unacceptable type"
         self.element.append(Element.convert_from_element(item))
-        db._save_key(self.db_key, db.folder)
+        
+        self.parent_dict.save_key(self.parent_key, db.folder)
 
 
 class Element_dict(Element):
@@ -135,9 +171,9 @@ class Element_dict(Element):
     Class for mutable dictionaries in the database
     """
 
-    def __init__(self, db_key, element):
-        assert type(element) == dict
-        super().__init__(db_key, element)
+    def __init__(self, parent_dict, parent_key, element, stop_parent_propagation=False):
+        assert isinstance(element, dict)
+        super().__init__(parent_dict, parent_key, element, stop_parent_propagation)
     
 
     def keys(self):
@@ -147,7 +183,7 @@ class Element_dict(Element):
         output:
             res: dict_keys
         """
-        return Element_dict_keys(self.element.keys(), self.db_key)
+        return Element_dict_keys(self.element.keys(), self.parent_dict, self.parent_key, self.stop_parent_propagation)
 
 
     def values(self):
@@ -157,7 +193,7 @@ class Element_dict(Element):
         output:
             res: dict_values
         """
-        return Element_dict_values(self.element.values(), self.db_key)
+        return Element_dict_values(self.element.values(), self.parent_dict, self.parent_key, self.stop_parent_propagation)
     
 
     def items(self):
@@ -167,7 +203,97 @@ class Element_dict(Element):
         output:
             res: dict_items
         """
-        return Element_dict_items(self.element.items(), self.db_key)
+        # items need both the dict and parent dict
+        # but if the parent dict is None, we can't get the normal dict from it
+        # so we pass the normal dict itself, and access the parent with the `parent_dict` attribute if needed
+        return Element_dict_items(self.element.items(), self, self.parent_key, self.stop_parent_propagation) 
+
+
+    def __setitem__(self, key, value):
+        assert db._check_if_acceptable(value), "Value could not be set because it contains an unacceptable type"
+        assert db._check_if_acceptable(key), "Key could not be set because it contains an unacceptable type"
+        self.element[key] = value
+        if self.stop_parent_propagation:
+            self.parent_dict.save_key(self.parent_key, db.folder)
+        else:
+            self.save_key(key, db.folder) 
+
+
+    def __delitem__(self, key) -> None:
+        del self.element[key]
+
+        os.remove(f"{self.get_folder_path(db.folder)}/{key}.dumped")
+
+            
+    def __getitem__(self, key):
+        if self.stop_parent_propagation:
+            return Element.convert_to_element(self.element[key], self.parent_dict, self.parent_key, self.stop_parent_propagation)
+        return Element.convert_to_element(self.element[key], self, key, self.stop_parent_propagation)
+    
+
+    def save(self, root_folder):
+        """
+        Saves the dictionary in the specified folder
+        If an item is also a dictionary, it will recursively be saved in a subfolder
+        -- 
+        input:
+            root_folder: str -> the folder where the dictionary will be saved
+        """
+        if not os.path.exists(root_folder):
+            os.mkdir(root_folder)
+
+        if len(self.element) == 0:
+            subfolder = self.get_folder_path(root_folder)
+            if not os.path.exists(subfolder):
+                os.mkdir(subfolder)
+            return
+
+        for key in self.keys():
+            self.save_key(key, root_folder)
+    
+
+    def save_key(self, key, root_folder):
+        """
+        Saves the value associated with a key in the specified folder
+        -- 
+        input:
+            key: str 
+            root_folder: str
+        """
+        if not db.is_loaded:
+            return
+
+        value = self[key]
+        subfolder = self.get_folder_path(root_folder)
+
+        if not os.path.exists(subfolder):
+            os.mkdir(subfolder)
+        
+        if isinstance(value, Element_dict):
+            value.save(root_folder)
+        else:
+            pickle.dump(Element.convert_from_element(value), open(f"{subfolder}/{key}.dumped", "wb"))
+    
+
+    def load(self, folder):
+        """
+        Loads the dictionary from the specified folder
+        --
+        input:
+            folder: str -> the folder where the dictionary will be loaded from
+        """
+        keys = os.listdir(folder)
+
+        if len(keys) == 0:
+            return
+
+        for key in keys:
+            if os.path.isdir(f"{folder}/{key}"):
+                d = Element_dict(self, key, {})
+                d.load(f"{folder}/{key}")
+                self[key] = Element.convert_from_element(d)
+            else:
+                self[key[:-7]] = pickle.load(open(f"{folder}/{key}", "rb"))
 
 
 class Element_dict_iterator:
@@ -175,10 +301,12 @@ class Element_dict_iterator:
     Class for the iterators generated from .keys(), .values() and .items() methods of Element_dict
     """
 
-    def __init__(self, collection, db_key):
+    def __init__(self, collection, parent_dict, parent_key, stop_parent_propagation=False):
         self.collection = collection
-        self.db_key = db_key
+        self.parent_dict = parent_dict
+        self.parent_key = parent_key
         self.iterator = None
+        self.stop_parent_propagation = stop_parent_propagation
     
 
     def __iter__(self):
@@ -187,7 +315,7 @@ class Element_dict_iterator:
 
 
     def __next__(self):
-        return Element.convert_to_element(next(self.iterator), self.db_key)
+        return Element.convert_to_element(next(self.iterator), self.parent_dict, self.parent_key, self.stop_parent_propagation)
     
 
     def __contains__(self, item):
@@ -213,7 +341,10 @@ class Element_dict_values(Element_dict_iterator):
 class Element_dict_items(Element_dict_iterator):
     def __next__(self):
         key, value = next(self.iterator)
-        return (Element.convert_to_element(key, self.db_key), Element.convert_to_element(value, self.db_key))
+        # Here, `parent_dict` is actually the child dict, so the actual parent is `parent_dict.parent_dict`
+        if self.stop_parent_propagation:
+            return (Element.convert_to_element(key, self.parent_dict.parent_dict, self.parent_key, self.stop_parent_propagation), Element.convert_to_element(value, self.parent_dict.parent_dict, self.parent_key, self.stop_parent_propagation))
+        return (Element.convert_to_element(key, self.parent_dict.parent_dict, self.parent_key, self.stop_parent_propagation), Element.convert_to_element(value, self.parent_dict, key, self.stop_parent_propagation))
 
 
 class My_database(dict):
@@ -225,6 +356,8 @@ class My_database(dict):
     ACCEPTABLE_TYPES_COLLECTION = [list, dict, Element_list, Element_dict]
 
     folder = None  # folder where the database is stored
+
+    is_loaded = False # prevents the database from writing before it is fully loaded
 
 
     def __init__(self, folder: str = None) -> None:
@@ -256,7 +389,7 @@ class My_database(dict):
     
 
     def __getitem__(self, key):
-        return Element.convert_to_element(super().__getitem__(key), key)
+        return Element.convert_to_element(super().__getitem__(key), self.to_element(), key, stop_parent_propagation=False)
 
 
     # set an item
@@ -264,7 +397,9 @@ class My_database(dict):
         assert self._check_if_acceptable(value), "Value could not be set because it contains an unacceptable type"
         assert self._check_if_acceptable(key), "Key could not be set because it contains an unacceptable type"
         super().__setitem__(key, Element.convert_from_element(value))
-        self._save_key(key, self.folder)
+        if self.is_loaded:
+            self.to_element().save_key(key, self.folder)
+        # self._save_key(key, self.folder)
 
 
     # delete an item
@@ -302,10 +437,9 @@ class My_database(dict):
         if folder is None:
             folder = self.folder
 
-        for key in self.keys():
-            self._save_key(key, folder)
-
-        
+        self.to_element().save(folder)
+    
+    
     def _load(self, folder: str = None) -> None:
         """
         Loads the database from the specified folder
@@ -316,44 +450,43 @@ class My_database(dict):
         if folder is None:
             folder = self.folder
 
-        keys = os.listdir(folder)
-        for key in keys:
-            self._load_key(key[:-7], folder)
+        d = self.to_element()
+        d.load(folder)
+
+        for key, val in d.items():
+            self[key] = val
+
+        self.is_loaded = True
     
-
-    def _save_key(self, key: str, folder: str) -> None:
-        """
-        Saves a key in the specified folder
-        --
-        input:
-            key: str -> the key to be saved
-            folder: str -> the folder where the key will be saved
-        """
-        pickle.dump(Element.convert_from_element(self[key]), open(f"{folder}/{key}.dumped", "wb"))
     
-
-    def _load_key(self, key: str, folder: str) -> None:
-        """
-        Loads a key from the specified folder
-        --
-        input:
-            key: str -> the key to be loaded
-            folder: str -> the folder where the key will be loaded from
-        """
-        self[key] = pickle.load(open(f"{folder}/{key}.dumped", "rb"))
+    def to_element(self):
+        return Element_dict(None, "", self)
 
 
-    
 
+try:
+    db = My_database()
+    db.folder = "D:/pibot/my_db"
+    db._load("my_db")
 
-    
+    # deletes all files and subfolders in the database folder
+    for file in os.listdir("my_db"):
+        os.remove(f"my_db/{file}")
+    db._save()
 
+except Exception as e:
+    db = My_database(folder="my_db")
 
-# db = My_database()
-# db.folder = "D:/pibot/my_db"
-# db._load("my_db")
+# from shutil import rmtree
 
-db = My_database(folder="my_db")
+# for file in os.listdir("test_db"):
+#     if os.path.isdir(f"test_db/{file}"):
+#         rmtree(f"test_db/{file}")
+#     else:
+#         os.remove(f"test_db/{file}")
+
+# a = pickle.load(open("D:/pibot/test_db/f.dumped", "rb"))
+# print(a)
 
 # for key in list(db.keys()):
 #     del db[key]
